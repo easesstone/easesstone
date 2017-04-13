99eba500bdbb612c3e0a6b202317418feefa82db
easesstone <576395455@qq.com>
2017-04-14 00:01:31 +0800

    tomcat7-plugin.properties and usage git add --all   and tomcat6-plugin.propertiest and usage

--- springmvc/springmvchelloworld/src/main/java/com/sydney/dream/handler/Helloworld.java
+++ springmvc/springmvchelloworld/src/main/java/com/sydney/dream/handler/Helloworld.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by Sydney on 2017/4/12.
  */
 @Controller
+@RequestMapping("/helloworld")
 public class Helloworld
 {
     /**
      * 使用@RequestMapping 处理请求的URL
+     * 既可以修饰方法，也可以修饰类
      * @return
      */
     @RequestMapping("/helloworld.do")
